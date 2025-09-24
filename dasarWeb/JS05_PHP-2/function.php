a6f31e0e64fde241c916f96fff87bb7ef1a6230f
nikytania <you@example.com>
2025-09-24 09:36:57 +0700

    Pemrograman Web

--- dasarWeb/JS05_PHP-2/function.php
+++ dasarWeb/JS05_PHP-2/function.php
@@ -1,10 +1,20 @@
 <?php
-function perkenalan() {
-  echo "Assalamualaikum, ";
-  echo "Perkenalkan, nama saya Niky<br/>";
-  echo "Senang berkenalan dengan Anda<br/>";
+//membuat fungsi
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
 }
 
-perkenalan();
+//memanggil fungsi yang sudah dibuat
+perkenalan("Hamdana", "Hallo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi
+perkenalan($saya, $ucapanSalam);
 
 ?>

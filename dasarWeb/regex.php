d9d7a6b0a1fcc3e441c5ef074db7eed8944f74f8
nikytania <you@example.com>
2025-10-08 09:12:51 +0700

    Pemrograman Web

--- dasarWeb/regex.php
+++ dasarWeb/regex.php
@@ -24,14 +24,14 @@ $text = '<br>I like apple pie.<br>';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text; // Output: "I like banana pie."
 
-$pattern = '/go?d/'; // "o" boleh muncul 0 atau 1 kali.
-$tests = ['gd', 'god', 'good'];
+$pattern = '/go{1,3}d/'; // "o" muncul minimal 1 kali dan maksimal 3 kali.
+$tests = ['gd', 'god', 'good', 'goood', 'goooood'];
 
 foreach ($tests as $text) {
   if (preg_match($pattern, $text, $matches)) {
-    echo $text . "==> Cocokkan: " . $matches[0] . "<br>";
+    echo $text . " => Cocokkan: " . $matches[0] . "<br>";
   } else {
-    echo $text . "==> Tidak ada yang cocok!<br>";
+    echo $text . " => Tidak cocok<br>";
   }
 }
 ?>

81f5885fff3c81bc433e79d2c19d124e91b70069
niky tania <nikytania0701@gmail.com>
2025-11-11 15:24:36 +0700

    Pemrograman Web

--- dasarWeb/JS12_OOP/oop.php
+++ dasarWeb/JS12_OOP/oop.php
@@ -1,113 +1,5 @@
 <?php
 
-//Basic OOP
-class Car
-{
-    public $brand;
-
-    public function startEngine()
-    {
-        echo "Engine started!<br>";
-    }
-}
-
-$car1 = new Car();
-$car1->brand = "Toyota";
-
-$car2 = new Car();
-$car2->brand = "Honda";
-
-$car1->startEngine();
-echo $car2->brand . "<br>";
-
-
-//Inheritance
-class Animal {
-    protected $name;
-
-    public function __construct($name) {
-        $this->name = $name;
-    }
-
-    public function eat() {
-        echo $this->name . " is eating.<br>";
-    }
-
-    public function sleep() {
-        echo $this->name . " is sleeping.<br>";
-    }
-}
-
-class Cat extends Animal {
-    public function meow() {
-        echo $this->name . " says meow!<br>";
-    }
-}
-
-class Dog extends Animal {
-    public function bark() {
-        echo $this->name . " says woof!<br>";
-    }
-}
-
-$cat = new Cat("Whiskers");
-$dog = new Dog("Buddy");
-
-$cat->eat();
-$dog->sleep();
-$cat->meow();
-$dog->bark();
-
-//Polymorphism
-interface Shape
-{
-    public function calculateArea();
-}
-
-class Circle implements Shape
-{
-    private $radius;
-    
-    public function __construct($radius)
-    {
-        $this->radius = $radius;
-    }
-    
-    public function calculateArea()
-    {
-        return pi() * pow($this->radius, 2);
-    }
-}
-
-class Rectangle implements Shape
-{
-    private $width;
-    private $height;
-    
-    public function __construct($width, $height)
-    {
-        $this->width = $width;
-        $this->height = $height;
-    }
-    
-    public function calculateArea()
-    {
-        return $this->width * $this->height;
-    }
-}
-
-function printArea(Shape $shape)
-{
-    echo "Area: " . $shape->calculateArea() . "<br>";
-}
-
-$circle = new Circle(5);
-$rectangle = new Rectangle(4, 6);
-
-printArea($circle);
-printArea($rectangle);
-
-
 //Encapsulation
 class Car
 {
@@ -144,4 +36,49 @@ echo "Color: " . $car->getColor() . "<br>";
 $car->setColor("Red");
 
 echo "Updated Color: " . $car->getColor() . "<br>";
+
+
+//Abstraction
+abstract class Shape
+{
+    abstract public function calculateArea();
+}
+
+class Circle extends Shape
+{
+    private $radius;
+    
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+    
+    public function calculateArea()
+    {
+        return pi() * pow($this->radius, 2);
+    }
+}
+
+class Rectangle extends Shape
+{
+    private $width;
+    private $height;
+    
+    public function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+    
+    public function calculateArea()
+    {
+        return $this->width * $this->height;
+    }
+}
+
+$circle = new Circle(5);
+$rectangle = new Rectangle(4, 6);
+
+echo "Area of Circle: " . $circle->calculateArea() . "<br>";
+echo "Area of Rectangle: " . $rectangle->calculateArea() . "<br>";
 ?>
